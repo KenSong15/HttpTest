fb4101eb3575ef37b0fa224813ba16f87182f71c
Ken <kensong96@gmail.com>
2019-07-12 16:58:53 -0400

    start js practise

---
+++ jsPractise/js/light.js
@@ -0,0 +1,9 @@
+function light(sw) {
+    var pic;
+    if (sw == 0) {
+      pic = "res/pic_bulboff.gif"
+    } else {
+      pic = "res/pic_bulbon.gif"
+    }
+    document.getElementById('myImage').src = pic;
+  }
